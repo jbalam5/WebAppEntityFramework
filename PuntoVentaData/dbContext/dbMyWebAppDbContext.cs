437a8f2ee37e38910b0089a22557301cb7e759c8
Jesus Noh <fnoh@nasa.com.mx>
2023-05-14 16:21:14 -0600

    Initial Commit

---
+++ PuntoVentaData/dbContext/dbMyWebAppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using PuntoVentaData.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuntoVentaData.dbContext
+{
+    public class dbMyWebAppDbContext: DbContext
+    {
+        public dbMyWebAppDbContext(string ConnectionString): base(ConnectionString)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Product>().ToTable("Product");
+        //    modelBuilder.Entity<Product>().ToTable("Category");
+        //}
+    }
+}
